31f5e1e1be8a533f245f7789ca8a156323bf3b25
Maciej Franikowski <mfranikowski@gmail.com>
2020-06-26 12:09:19 +0200

    Generated materials dictionary

--- automateMaterials.py
+++ automateMaterials.py
@@ -1,10 +1,7 @@
 import openpyxl
 import os
 import re
-import csubDictionary
-import sortListFunction
-import pyautogui
-import time
+import pprint
 
 csubValueRegex = re.compile(r'(\d.(\d)?\d.\d(\d)?)')
 unsortedCsubValues = []
@@ -15,52 +12,23 @@ xMousePosition = 1029
 yMousePosition = 272
 startingY = 272
 
-print('Give excel file current directory')
+print('Give materials excel file current directory')
 excelDirectory = input()
 os.chdir(excelDirectory)
 
 print('Give the name of the file (without the extension)')
 fileName = input()
 workBook = openpyxl.load_workbook(fileName + '.xlsx')
-workSheet = workBook['Szczegółowy bez mat.']
-firstColumnList = list(workSheet.columns)[0]
+workSheet = workBook['Sheet1']
+#productCode = list(workSheet.columns)[1]
+#print(productCode)
+#input()
 
-# Adding the values from the first column of the excel file to the
-# unsorted list that'll contatin all csub values.
-for cellObject in firstColumnList:
-    mo = csubValueRegex.search(str(cellObject.value))
-    if(mo is not None and mo.group() not in unsortedCsubValues):
-        unsortedCsubValues.append(mo.group())
+dictonary = {}
+for row in workSheet.iter_rows(min_row=1, max_col=3, max_row=96, values_only = True):
+    t = tuple(row[:2])
+    dictonary[row[2]] = t
 
-
-sortedCsubValues = sortListFunction.sortList(unsortedCsubValues)
-for strings in sortedCsubValues:
-    print(strings)
-    print(csubDictionary.csub.get(strings))
-
-time.sleep(5)
-pyautogui.moveTo(xMousePosition, yMousePosition)
-
-for strings in sortedCsubValues:
-    # Calculate the difference between the ordering number of the Csub Value
-    # and the current position in the SAP list.
-    differencePosition = csubDictionary.csub.get(strings) - currentPosition
-    #
-    difference = differencePosition * 27
-    currentPosition += differencePosition
-    print('differencePosition = ' + str(differencePosition))
-    print('currentPosition - currentSliderPosition ' + str(currentPosition - currentSliderPosition))
-    if(currentPosition - currentSliderPosition >= 25):
-        yMousePosition = startingY - 20
-        pyautogui.moveTo(xMousePosition, sliderY)
-        sliderY += (currentPosition - currentSliderPosition) * 2.88888888
-        pyautogui.drag(0, (currentPosition - currentSliderPosition) * 2.888888888, duration = 0.2)
-        time.sleep(3)
-        currentSliderPosition = currentPosition
-        pyautogui.click(30, yMousePosition)
-    else:
-        pyautogui.click(30, yMousePosition + difference)
-        yMousePosition += difference
-
-    print('Current Position = ' + str(currentPosition))
-    print('Current Slider Position = ' + str(currentSliderPosition) + '\n')
+fileObj = open('materialsDictionary.py', 'w')
+fileObj.write('materials = ' + pprint.pformat(dictonary) + '\n')
+fileObj.close()
